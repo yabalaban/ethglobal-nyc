12d111c63857e88ae0a47d88ede35a141d9029b3
Alexander Balaban <balaban.alexander+github@gmail.com>
2023-09-23 05:13:41 -0400

    air stach and graph integrations

---
+++ apps/snap/packages/snap/src/constants.ts
@@ -0,0 +1,9 @@
+export const AIRSTACK_ENDPOINT = 'https://api.airstack.xyz/gql';
+export const AIRSTACK_KEY = 'c017255cba7c4e199917a94addfdb682';
+export const GRAPH_QUERY_URL = 'https://api.studio.thegraph.com/query/53551/sqam-testnet-v1/version/latest';
+
+export const ACCEPTED_TOKENS = {
+    "0xdAC17F958D2ee523a2206206994597C13D831ec7": "USDT",
+    "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48": "USDC",
+  };
+  
--- apps/snap/packages/snap/src/index.ts
+++ apps/snap/packages/snap/src/index.ts
@@ -1,59 +1,49 @@
-import type { OnTransactionHandler } from '@metamask/snaps-types';
-import { panel, text } from '@metamask/snaps-ui';
-import { init } from "@airstack/node";
+import { OnTransactionHandler } from '@metamask/snaps-types';
+import { panel, text, heading } from '@metamask/snaps-ui';
+import { GetBalances, GetDomainReporters, GetRecipientReporters } from './queries';
 
-export const AIRSTACK_ENDPOINT = 'https://api.airstack.xyz/gql';
-export const AIRSTACK_KEY = 'c017255cba7c4e199917a94addfdb682';
+const keccak256 = require('keccak256');
 
-export const AcceptedTokens = {
-    "0xdAC17F958D2ee523a2206206994597C13D831ec7": "USDT",
-    "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48": "USDC",
-};
-
-export const Query = `query Query($owners: [Identity!], $tokenAddress: [Address!]) {
-  TokenBalances(
-    input: {filter: {owner: {_in: $owners}, tokenAddress: {_in: $tokenAddress}}, blockchain: ethereum}
-  )
-  {
-    TokenBalance {
-      amount
-      tokenAddress
-    }
-  }
-}`
-
-async function fetchQuery(query: string, variables: Record<string, any>) {
-  let results = await fetch(AIRSTACK_ENDPOINT, {
-    method: 'POST',
-
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": "c017255cba7c4e199917a94addfdb682"
-    },
-
-    body: JSON.stringify({
-      query,
-      variables,
-    }),
-  })
-  let characters = await results.json();
-  return characters.data
+function scoreAddressReporter(balance: number[]): number {
+  return 0.5;
 }
 
-export async function GetScoring(addresses: string[]): Promise<string | undefined> {
-    const data = await fetchQuery(Query, {owners: addresses, tokenAddress: Object.keys(AcceptedTokens)});
-    console.log(JSON.stringify(data));
-    return JSON.stringify(data);
+function scoreDomainReporter(balance: number[]): number {
+  return 0.5;
 }
 
 export const onTransaction: OnTransactionHandler = async ({ transaction, transactionOrigin }) => {
-  // 1. [Snap] hash origin
-  // 2. [Subgraph] query reporter addresses per recipient and origin
-  // 3. [Airstack] reporter stats
-  // 4. [Snap] local scoring 
-  // 5. [Snap] insight (severe or normal) based on the score
-  const data = await GetScoring(["yabalaban.eth"]);
-  return { content: panel([text(`**Transaction type: ${data} **`)]) };
+  const recipient = transaction.to as string; 
+  const reporters1 = await GetRecipientReporters(recipient);
+  
+  const domainHash = `0x${keccak256(transactionOrigin).toString('hex')}`;
+  const reporters2 = await GetDomainReporters(domainHash);
+  
+  const balances1 = await GetBalances(reporters1);
+  const addressReportersBalance = reporters1.map(function(r, i) {
+    return [r, balances1[i]];
+  });
+
+  const balances2 = await GetBalances(reporters2.map((function(r, i) { return r[0]; })));
+  const domainReportersBalance = reporters1.map(function(r, i) {
+    return [r, balances1[i]];
+  });
+
+  const score1 = scoreAddressReporter(balances1);
+  const score2 = scoreDomainReporter(balances2);
+  const totalScore = 0.2 * score1 + 0.8 * score2;
+    
+  if (totalScore < 0.3) {
+    // high severity, low score
+    return { content: [text(`**Transaction type: low score **`)] };
+  } else if (totalScore > 0.7) {
+    // info level, high score
+    return { content: panel([text(`**Transaction type: high score **`)]) };
+  } else {
+    // info level, no signal
+    return { content: panel([text(`**Transaction type: middle score **`)]) };
+  }
 };
 
-// GetScoring(["yabalaban.eth"]);
+
+GetBalances(["yabalaban.eth"])
