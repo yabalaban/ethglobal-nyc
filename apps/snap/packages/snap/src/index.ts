9e08532d5cc1870f6096498e1ac0e92b00cbce12
Alexander Balaban <balaban.alexander+github@gmail.com>
2023-09-23 02:01:42 -0400

    [airstack] scoring

--- apps/snap/packages/snap/src/index.ts
+++ apps/snap/packages/snap/src/index.ts
@@ -1,6 +1,50 @@
 import type { OnTransactionHandler } from '@metamask/snaps-types';
 import { panel, text } from '@metamask/snaps-ui';
-import { GetScoring } from "../../../../scoring/src"
+import { init } from "@airstack/node";
+
+export const AIRSTACK_ENDPOINT = 'https://api.airstack.xyz/gql';
+export const AIRSTACK_KEY = 'c017255cba7c4e199917a94addfdb682';
+
+export const AcceptedTokens = {
+    "0xdAC17F958D2ee523a2206206994597C13D831ec7": "USDT",
+    "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48": "USDC",
+};
+
+export const Query = `query Query($owners: [Identity!], $tokenAddress: [Address!]) {
+  TokenBalances(
+    input: {filter: {owner: {_in: $owners}, tokenAddress: {_in: $tokenAddress}}, blockchain: ethereum}
+  )
+  {
+    TokenBalance {
+      amount
+      tokenAddress
+    }
+  }
+}`
+
+async function fetchQuery(query: string, variables: Record<string, any>) {
+  let results = await fetch(AIRSTACK_ENDPOINT, {
+    method: 'POST',
+
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "c017255cba7c4e199917a94addfdb682"
+    },
+
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  })
+  let characters = await results.json();
+  return characters.data
+}
+
+export async function GetScoring(addresses: string[]): Promise<string | undefined> {
+    const data = await fetchQuery(Query, {owners: addresses, tokenAddress: Object.keys(AcceptedTokens)});
+    console.log(JSON.stringify(data));
+    return JSON.stringify(data);
+}
 
 export const onTransaction: OnTransactionHandler = async ({ transaction, transactionOrigin }) => {
   // 1. [Snap] hash origin
@@ -9,4 +53,7 @@ export const onTransaction: OnTransactionHandler = async ({ transaction, transac
   // 4. [Snap] local scoring 
   // 5. [Snap] insight (severe or normal) based on the score
   const data = await GetScoring(["yabalaban.eth"]);
+  return { content: panel([text(`**Transaction type: ${data} **`)]) };
 };
+
+// GetScoring(["yabalaban.eth"]);
