49aa7a2e699758af628be4844543c5d5909fd5ad
Alexander Balaban <balaban.alexander+github@gmail.com>
2023-09-22 23:26:53 -0400

    switch to transaction insights type of snap

--- apps/snap/packages/snap/src/index.ts
+++ apps/snap/packages/snap/src/index.ts
@@ -1,33 +1,11 @@
-import { OnRpcRequestHandler } from '@metamask/snaps-types';
+import type { OnTransactionHandler } from '@metamask/snaps-types';
 import { panel, text } from '@metamask/snaps-ui';
 
-/**
- * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
- *
- * @param args - The request handler args as object.
- * @param args.origin - The origin of the request, e.g., the website that
- * invoked the snap.
- * @param args.request - A validated JSON-RPC request object.
- * @returns The result of `snap_dialog`.
- * @throws If the request method is not valid for this snap.
- */
-export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
-  switch (request.method) {
-    case 'hello':
-      return snap.request({
-        method: 'snap_dialog',
-        params: {
-          type: 'confirmation',
-          content: panel([
-            text(`Hello, **${origin}**!`),
-            text('This custom confirmation is just for display purposes.'),
-            text(
-              'But you can edit the snap source code to make it do something, if you want to!',
-            ),
-          ]),
-        },
-      });
-    default:
-      throw new Error('Method not found.');
-  }
+export const onTransaction: OnTransactionHandler = async ({ transaction, transactionOrigin }) => {
+  // 1. [Snap] hash origin
+  // 2. [Subgraph] query reporter addresses per recipient and origin
+  // 3. [Airstack] reporter stats
+  // 4. [Snap] local scoring 
+  // 5. [Snap] insight (severe or normal) based on the score
+  return { content: panel([text('Simple message')]) };
 };
